2b45b9f4071e5ce0ee6b74189ebfdda6b3c8d50e
HERVEBORISMBOULONTSI <mherveboris@gmail.com>
2025-12-29 18:14:49 +0100

    feat: init qcm project with nestjs react and k8s

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,13 @@
 CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100)
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(150) NOT NULL UNIQUE
 );
 
-INSERT INTO users (name)
-VALUES ('Hervé DevOps')
-ON CONFLICT DO NOTHING;
+INSERT INTO users (name, email)
+VALUES 
+  ('Alice', 'alice@example.com'),
+  ('Bob', 'bob@example.com'),
+  ('Charlie', 'charlie@example.com'),
+  ('Herve', 'herve@devops.com')
+ON CONFLICT (email) DO NOTHING;
